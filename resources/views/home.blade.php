44cee12687f719464d51b607823cbf5d912e3641
Angelique-Smit <ajd.smit@gmail.com>
2023-09-22 17:08:34 +0200

    Database users + projects added
    Added:
    
    -users table
    -projects table'
    
    HTML:
    -Added a home and project blade.php file
    -Small bit of css. Does not show up yet.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home</title>
+    <link rel="stylesheet" type="text/css" href="../css/home.css">
+</head>
+<body>
+<nav>
+    <section id="nav_container">
+        <div class="link"> <img src="" alt="Sewables logo"> </div>
+        <div class="link"> <a href="home.blade.php"> Home </a> </div>
+        <div class="link"> <a href="projects.blade.php"> Projects </a> </div>
+        <div class="dropdown">
+            <button class="dropbtn">Dropdown</button>
+            <div class="dropdown-content">
+                <a href="#">Log in</a>
+            </div>
+        </div>
+    </section>
+</nav>
+</body>
+</html>
+
