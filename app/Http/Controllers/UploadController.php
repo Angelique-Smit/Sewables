8842a9bd6bb052b9e52963436db519508d34426f
Angelique-Smit <ajd.smit@gmail.com>
2023-10-14 13:55:01 +0200

    Made form, looking into laravel validation

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse; // Add this import
+use App\Http\Requests\StoreProjectPost; // Add this import
 
 class UploadController extends Controller
 {
@@ -10,4 +12,14 @@ class UploadController extends Controller
     {
         return view('upload');
     }
+
+    public function store(StoreProjectPost $request): RedirectResponse
+    {
+        // Retrieve the validated input data...
+        $validated = $request->validated();
+
+        Post::create($validated);
+        return redirect('/');
+    }
 }
+
--- database/migrations/2023_09_22_121958_projects.php
+++ database/migrations/2023_09_22_121958_projects.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
             $table->foreign('user_id')->references('id')->on('users');
+            $table->text('title');
             $table->string('picture_url');
             $table->text('description');
             $table->text('explanation');
